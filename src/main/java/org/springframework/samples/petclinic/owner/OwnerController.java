a5740a21f758f4ce72e12308a8a7d54d9a0bb060
Radim Pavlicek <radim.pavlicek@gmail.com>
2024-08-29 15:06:38 +0200

    skoda CRU

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -38,13 +38,6 @@ import org.springframework.web.servlet.ModelAndView;
 import jakarta.validation.Valid;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-/**
- * @author Juergen Hoeller
- * @author Ken Krebs
- * @author Arjen Poutsma
- * @author Michael Isvy
- * @author Wick Dynex
- */
 @Controller
 class OwnerController {
 
