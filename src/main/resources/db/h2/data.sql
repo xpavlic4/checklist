0d5ca5f109dcba791ee33f8d369c73d7c0481974
Radim Pavlicek <radim.pavlicek@gmail.com>
2024-10-04 15:17:00 +0200

    data from Libor

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -20,6 +20,7 @@ INSERT INTO types VALUES (default, 'Protiargument');
 
 
 INSERT INTO owners VALUES (default, 'Libor', 'Pavlicek', '123 Hollywood', 'Los Angeles', '6085551023');
+INSERT INTO owners VALUES (default, 'Adam', 'Adam', '123 Hollywood', 'Los Angeles', '6085551023');
 --INSERT INTO owners VALUES (default, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749');
 --INSERT INTO owners VALUES (default, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFarland', '6085558763');
 --INSERT INTO owners VALUES (default, 'Harold', 'Davis', '563 Friendly St.', 'Windsor', '6085553198');
@@ -28,12 +29,32 @@ INSERT INTO owners VALUES (default, 'Libor', 'Pavlicek', '123 Hollywood', 'Los A
 INSERT INTO pets VALUES (default, 'Chodec věnoval pozornost','Díval se na cestu před sebou', '2010-09-07', 1, 1);
 INSERT INTO pets VALUES (default, 'Obec porušila standard řádné péče','Neměla zpracovaný plán údržby', '2010-09-07', 1, 1);
 INSERT INTO pets VALUES (default, 'Porušil povinnost prevence','Neměl vhodnou obuv.', '2010-09-07', 1, 1);
---INSERT INTO pets VALUES (default, 'Basil', '2012-08-06', 3, 2);
---INSERT INTO pets VALUES (default, 'Rosy', '2011-04-17', 2, 2);
---INSERT INTO pets VALUES (default, 'Jewel', '2010-03-07', 2, 1);
---
+INSERT INTO pets VALUES (default, 'Byla výluka z plnění plánu údržby','čas pádu nastal v době, kdy údržba nemohla zajistit odpovídající stav dle plánu údržby v přiměřené lhůtě.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Byla výstraha,','ČHMÚ varoval v den pádu o riziku námrazy.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Chodec byl fyzicky i psychicky způsobilý,','se jedná o běžného čtyřicátníka, který šel ráno do práce.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Chodec věnoval pozornost','se díval před sebe pod nohy.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Obec měla zpracovaný plán údržby','protože byl schválen veřejnou vyhláškou.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Obuv byla vhodná','podrážka měla hrubý vzorek', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Stav chodníku byl vyhovující','v zimě bývá obvykle sníh a led všude.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Stav chodníku nebyl vyhovující','bylo náledí.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Zvolená trasa byla nebezpečná','obsahovala změnu terénu.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Zvolená trasa byla riziková','trasa obsahovala místo vedoucí kolem kontejnerů bez pokrytí úklidem a s viditelnými zmrazky.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Zvolená trasa byla známá','tudy chodec chodil do práce.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Zvolená trasa nebyla bezpečná','vedla šikmo z vozovky.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Závada byla překvapivá','protože neošetřená část byla zjistitelná jen se zvýšeným vizuálním úsilím.', '2010-09-07', 1, 2);
+INSERT INTO pets VALUES (default, 'Závada nebyla ojedinělá','v danou chvíli byla kluzká celá ulice', '2010-09-07', 1, 2);
+
+
 INSERT INTO visits VALUES (default, 1, '2013-01-01', 'Nevěnoval pozornost protože se díval do mobilu');
 INSERT INTO visits VALUES (default, 2, '2013-01-02', 'Měla plán údržby, který byl schválený zastupitelstvem pod 23/2012.');
 INSERT INTO visits VALUES (default, 3, '2013-01-03', 'Neporusil povinnost presence');
 INSERT INTO visits VALUES (default, 3, '2013-01-04', 'Mit vhodnou obuv neni obsahem povinnosti prevence.');
 INSERT INTO visits VALUES (default, 3, '2013-01-04', 'Obuv měla hrubou podrážku.');
+INSERT INTO visits VALUES (default, 4, '2013-01-04', 'Nebyla výluka, protože v době pádu již měl být dle plánu údržby stav odpovídající schůdnosti a lhůta k zajištění stavu byla přiměřená.');
+INSERT INTO visits VALUES (default, 5, '2013-01-04', 'Nebyla výstraha, protože varování ČHMÚ nebylo zveřejněno ve zpravodajství.');
+INSERT INTO visits VALUES (default, 6, '2013-01-04', 'Chodec nebyl způsobilý, protože to ráno zaspal, byl rozespalý, byl unavený a chvatně pospíchal do práce.');
+INSERT INTO visits VALUES (default, 7, '2013-01-04', 'Chodec nevěnoval pozornost, protože zrovna používal mobil.');
+INSERT INTO visits VALUES (default, 8, '2013-01-04', 'Obec neměla plán údržby, protože ve skutečnosti podle něj nepostupovala a plán údržby byl pouze formální (tzv. na papíře).');
+INSERT INTO visits VALUES (default, 8, '2013-01-04', 'Obec měla plán údržby, protože pluhovala a solila chodníky.');
+INSERT INTO visits VALUES (default, 9, '2013-01-04', 'Obuv nebyla vhodná, protože boty byly staré a podrážka byla značně opotřebovaná.');
+INSERT INTO visits VALUES (default, 17, '2013-01-04', 'Závada byla ojedinělá, protože část chodníků v danou chvíli již byla ošetřena a pouze část v místě nehody nebyla.');
