e3848d64f75fe6638aa09b2678ead0229600ac6f
Radim Pavlicek <radim.pavlicek@gmail.com>
2024-10-10 12:39:28 +0200

    formatting

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -27,21 +27,21 @@ import jakarta.validation.constraints.NotBlank;
 @MappedSuperclass
 public class Person extends BaseEntity {
 
-//	@Column(name = "first_name")
-//	@NotBlank
-//	private String firstName;
+	// @Column(name = "first_name")
+	// @NotBlank
+	// private String firstName;
 
 	@Column(name = "last_name")
 	@NotBlank
 	private String lastName;
 
-//	public String getFirstName() {
-//		return this.firstName;
-//	}
-//
-//	public void setFirstName(String firstName) {
-//		this.firstName = firstName;
-//	}
+	// public String getFirstName() {
+	// return this.firstName;
+	// }
+	//
+	// public void setFirstName(String firstName) {
+	// this.firstName = firstName;
+	// }
 
 	public String getLastName() {
 		return this.lastName;
