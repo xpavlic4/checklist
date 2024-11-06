d6fa80fa1f786308d2362bcbfee103883b7ec3e1
Radim Pavlicek <radim.pavlicek@gmail.com>
2024-11-06 20:38:29 +0100

    Add social login functionality
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/xpavlic4/checklist?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/main/java/org/springframework/samples/petclinic/controller/LoginController.java
+++ src/main/java/org/springframework/samples/petclinic/controller/LoginController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequestMapping("/login")
 public class LoginController {
