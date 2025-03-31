76fb487b9f0cb86bc5b7621dd6a30258ca5a03db
Radim Pavlicek <radim.pavlicek@gmail.com>
2025-03-31 21:29:46 +0200

    backup sql files

---
+++ src/main/resources/db/backup/visits.sql
@@ -0,0 +1,16 @@
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (1, 1, '2013-01-01', 'Nevěnoval pozornost protože se díval do mobilu', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (2, 2, '2013-01-02', 'Měla plán údržby, který byl schválený zastupitelstvem pod 23/2012.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (3, 3, '2013-01-03', 'Neporusil povinnost presence', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (4, 3, '2013-01-04', 'Mit vhodnou obuv neni obsahem povinnosti prevence.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (5, 3, '2013-01-04', 'Obuv měla hrubou podrážku.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (6, 4, '2013-01-04', 'Nebyla výluka, protože v době pádu již měl být dle plánu údržby stav odpovídající schůdnosti a lhůta k zajištění stavu byla přiměřená.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (7, 5, '2013-01-04', 'Nebyla výstraha, protože varování ČHMÚ nebylo zveřejněno ve zpravodajství.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (8, 6, '2013-01-04', 'Chodec nebyl způsobilý, protože to ráno zaspal, byl rozespalý, byl unavený a chvatně pospíchal do práce.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (9, 7, '2013-01-04', 'Chodec nevěnoval pozornost, protože zrovna používal mobil.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (10, 8, '2013-01-04', 'Obec neměla plán údržby, protože ve skutečnosti podle něj nepostupovala a plán údržby byl pouze formální (tzv. na papíře).', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (11, 8, '2013-01-04', 'Obec měla plán údržby, protože pluhovala a solila chodníky.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (12, 9, '2013-01-04', 'Obuv nebyla vhodná, protože boty byly staré a podrážka byla značně opotřebovaná.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (13, 17, '2013-01-04', 'Závada byla ojedinělá, protože část chodníků v danou chvíli již byla ošetřena a pouze část v místě nehody nebyla.', null, null);
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (14, 25, '2024-12-20', null, 'predicate test', 'premise test');
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (15, 27, '2025-02-26', null, 'Délka nebyla nepřiměřená', 'trvala méně než 2 roky.');
+INSERT INTO public.visits (id, pet_id, visit_date, description, predicate, premise) VALUES (16, 27, '2025-02-26', null, 'Nárok není důvodný', 'řízení probíhalo na třech stupních.');
