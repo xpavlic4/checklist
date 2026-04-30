bbf8f4ec97d0273d1b712944e112bc7c042cbd32
Radim Pavlicek <radim.pavlicek@gmail.com>
2026-04-30 15:37:38 +0200

    /favicon.ico

--- src/main/java/org/springframework/samples/petclinic/config/SecurityConfig.java
+++ src/main/java/org/springframework/samples/petclinic/config/SecurityConfig.java
@@ -16,8 +16,8 @@ public class SecurityConfig {
 			throws Exception {
 		http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
 			.requestMatchers("/login", "/oauth2/**", "/webjars/**", "/resources/**", "/actuator/health",
-					"/h2-console/**", "favicon.ico", "/**/*.css", "/**/*.js", "/**/*.map", "/css/**", "/js/**",
-					"/error/**", "/images/**")
+					"/h2-console/**", "/favicon.ico", "/**/*.css", "/**/*.js", "/**/*.map", "/css/**", "/js/**",
+					"/error/**", "/images/**", "/oups")
 			.permitAll()
 			.anyRequest()
 			.authenticated())
