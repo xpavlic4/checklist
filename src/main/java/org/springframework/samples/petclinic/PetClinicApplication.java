2a93fa9c97cd21a6bd763af94d77a66567f7461d
Radim Pavlicek <radim.pavlicek@gmail.com>
2026-06-12 17:40:54 +0200

    remove petclinic code

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -26,7 +26,6 @@ import org.springframework.context.annotation.ImportRuntimeHints;
  * @author Dave Syer
  */
 @SpringBootApplication
-@ImportRuntimeHints(PetClinicRuntimeHints.class)
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
